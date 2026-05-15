33338bd86d920c4e0cf15ff85b5910f2a892e50e
kumar <kumarajay912282@gmail.com>
2026-05-15 19:34:34 +0530

    Add output for second element in Array_02 main method

--- Array_02.java
+++ Array_02.java
@@ -3,5 +3,6 @@ public class Array_02 {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50}; 
         System.out.println(arr[0]);
+        System.out.println(arr[1]);
     }
 }
