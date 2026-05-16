5cef0add16ce1bbf255c27e897c86627e9d3b9e1
kumar <kumarajay912282@gmail.com>
2026-05-16 11:38:52 +0530

    Add initial implementation of Arraay_05 class with main method for jagged array demonstration

---
+++ Arraay_05.java
@@ -0,0 +1,14 @@
+public class Arraay_05 {
+    // 2D array with different column sizes that is jaggged arra
+    public static void main(String[] args) {
+        int [][] a = new int[2][];
+        a[0] = new int[3];
+        a[1] = new int[2];
+        a[0][0] = 1;
+        a[0][1] = 2;  
+        a[0][2] = 3;
+        a[1][0] = 4;  
+        a[1][1] = 5;
+        System.out.println(a[0][0]);
+    }
+}
