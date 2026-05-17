0959ea6cf6fa5467b8e848e49e6767ac48bff55d
kumar <kumarajay912282@gmail.com>
2026-05-17 11:25:42 +0530

    Add initial implementation of Array_09 class with main method for 3D array retrieval

---
+++ Array_09.java
@@ -0,0 +1,15 @@
+public class Array_09 {
+
+    //retrive all the values from 3D array
+    public static void main(String[] args) {
+         int [][][] a = {{{10, 20, 30}, {40, 50}}, {{60, 70}}};
+         
+         for(int i = 0; i < a.length; i++) {
+             for(int j = 0; j < a[i].length; j++) {
+                 for(int k = 0; k < a[i][j].length; k++) {
+                     System.out.print(a[i][j][k] + " ");
+                 }
+             }
+         }
+    }
+}
