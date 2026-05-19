ff58cff325e221b613d07443520ba8296b13407f
kumar <kumarajay912282@gmail.com>
2026-05-19 17:24:24 +0530

    Fix variable name in Array_10 class for consistency and clarity

--- Array_10.java
+++ Array_10.java
@@ -6,18 +6,16 @@ public class Array_10 {
       System.out.println("enter size of an array: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[]age = new int[n];
+        int[]a = new int[n];
         System.out.println("enter element of an array: ");
         for(int i=0; i<n;i++){
-          age[i]= sc.nextInt();
- 
+          a[i]= sc.nextInt();
         }
-      System.out.println("Print array: ");
+          System.out.println("Print array: ");
         for(int i=0; i<n;i++){
-            System.out.print(age[i]+ " ");
+            System.out.print(a[i]+ " ");
  
         }
-    }
-    
-    
+        sc.close();
+    }  
 }
