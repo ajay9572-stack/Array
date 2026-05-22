7d3b4eec76231e556ff3f81113debbf553fd17bb
kumar <kumarajay912282@gmail.com>
2026-05-22 18:54:54 +0530

    Change print statement for user input prompt in Array_16 class

--- Array_16.java
+++ Array_16.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 // find index no in an array
 public class Array_16 {
     public static void main(String[] args) {
-        System.out.println("enter row: ");
+        System.out.print("enter row: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
          int []a = new int[n];
