e346d71394fb10067bdb96812cec9b930643ae29
kumar <kumarajay912282@gmail.com>
2026-05-24 20:36:16 +0530

    Fix output prompt to use println for displaying the reverse of the array in Array_19 class

--- Array_19.java
+++ Array_19.java
@@ -18,7 +18,7 @@ public class Array_19  {
               arr[j]=a[i];
               j++;
         }
-        System.out.print("reverse of array:");
+        System.out.println("reverse of array:");
        for(int i=0;i<a.length;i++){
         System.out.println(arr[i]);
         
