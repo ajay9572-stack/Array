fc7bd018e3c6d5c1e0798c6c8b58832bbb8bdb07
kumar <kumarajay912282@gmail.com>
2026-05-25 19:32:20 +0530

    Add Array_20 class to calculate the sum of an array elements

---
+++ Array_20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+           // sum of an array//
+public class Array_20 {
+    
+   public static void main(String[] args) {
+    System.out.println("enter size of an array: ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] number = new int[n];
+    int sum = 0;
+    System.out.println("enter elemnt of an array: ");
+    for( int i = 0; i<n;i++){
+       number[i] = sc.nextInt();
+
+    }
+ for(int i =0; i<n;i++){
+    sum = sum+ number[i];
+    
+ }
+
+//  System.out.print( sum);
+}
+
+}
