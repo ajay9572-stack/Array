7ce6f38b0df8b9351defef78f7cc116227834930
kumar <kumarajay912282@gmail.com>
2026-05-26 21:44:21 +0530

    Add Array_21 class to find the maximum value in an array

---
+++ Array_21.java
@@ -0,0 +1,28 @@
+ import java.util.Scanner;
+           // max value of an array//
+
+public class Array_21 {
+   
+
+    public static void main(String[] args) {
+        System.out.println("enter size of an array:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] number = new int[n];
+        int maximumvalue = 0;
+        System.out.println("enter element of an array:");
+        for (int i =0; i<n;i++){
+           number [i] = sc.nextInt();
+        }
+        for (int i=  0; i<n; i++){
+           if (number[i]>maximumvalue){
+               maximumvalue = number[i];
+            }
+
+        }
+      //  System.out.println("print maximum element of an araray: ");
+        System.out.println(maximumvalue);
+    }
+}
+
+
